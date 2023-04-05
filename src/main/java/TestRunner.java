217195e6468ed8c927c98cddbbd71ad869e81a74
sumcris03 <cristinasumurduc03@gmail.com>
2023-04-05 13:26:38 +0300

    sarcina15-fin

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -1,9 +1,31 @@
 import Managers.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static java.lang.Thread.sleep;
 
 public class TestRunner {
     public static void main(String[] args ) {
         WebDriverManager webDriverManager=new WebDriverManager ("CHROME");
-        webDriverManager.getDriver().get("https://www.google.md/");
+        webDriverManager.getDriver().get("https://demo.opencart.com/");
+
+        WebElement myAcountButton = webDriverManager.getDriver().findElement(By.xpath("//*[@id=\"carousel-banner-0\"]/div[2]/div[2]"));
+        myAcountButton.click();
+
+        WebElement registerButton = webDriverManager.getDriver().findElement(By.xpath("//*[@id=\"carousel-banner-0\"]/button[2]"));
+        registerButton.click();
+
+
+        WebElement firstNameInput =webDriverManager.getDriver().findElement(By.name("common-home-firstNameInput"));
+        firstNameInput.sendKeys("First Text Input");
+
+        try {
+            sleep(2500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
         webDriverManager.getDriver().close();
 
 
